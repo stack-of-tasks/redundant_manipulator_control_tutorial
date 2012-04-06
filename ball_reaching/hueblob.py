9fd07953a8422ad9ac4179c4aade471db54d6c49
Florent Lamiraux <florent@laas.fr>
2012-04-06 13:01:28 +0200

    Use rosbag for simulation

--- ball_reaching/hueblob.py
+++ ball_reaching/hueblob.py
@@ -9,8 +9,9 @@ import tf
 from geometry_msgs.msg import TransformStamped
 
 def publish () :
-    cameraName = "camera_top_right_optical"
-    rospy.init_node ('tf_hueblob_simu')
+    print ("Starting hueblob")
+    cameraName = "camera_bottom_left_optical"
+    rospy.init_node ('tf_hueblob')
     pub_world = rospy.Publisher ('/wide/blobs/rose/transform_world',
                                  TransformStamped)
     pub_camera = rospy.Publisher ('/wide/blobs/rose/transform',
@@ -21,42 +22,35 @@ def publish () :
     M = TransformStamped ()
     rate = rospy.Rate (10)
     while not rospy.is_shutdown ():
-        # Position of rose ball as a tf
-        x = 0.4; y = -0.2; z = 1.2
-        broadcaster.sendTransform ((x, y, z), (0., 0., 0., 1.),
-                                   rospy.Time.now (),
-                                   "rose_ball", "world")
-        # Position of rose ball in world frame as a topic
-        M.header.stamp = rospy.Time.now ()
-        M.transform.translation.x = x
-        M.transform.translation.y = y
-        M.transform.translation.z = z
-        M.transform.rotation.x = 0.0
-        M.transform.rotation.y = 0.0
-        M.transform.rotation.z = 0.0
-        M.transform.rotation.w = 1.0
-        pub_world.publish (M)
-
-        rate.sleep ()
-
-        if listener.frameExists("rose_ball") and \
-                listener.frameExists(cameraName):
+        if listener.frameExists("roseball") and \
+                listener.frameExists("world"):
             try:
-                t = listener.getLatestCommonTime("rose_ball", cameraName)
+                t = listener.getLatestCommonTime("roseball", "world")
                 (translation, quaternion) = \
-                    listener.lookupTransform(cameraName, "rose_ball",
-                                             t)
+                    listener.lookupTransform("world", "roseball", t)
+                print ("t={0}".format (t))
+                broadcaster.sendTransform (translation, quaternion,
+                                           rospy.Time.now (),
+                                           "roseball", "world")
+
+                # Position of rose ball in world frame as a topic
+                M.header.stamp = rospy.Time.now ()
                 (M.transform.translation.x,
                  M.transform.translation.y,
                  M.transform.translation.z) = translation
-                print ("translation = %s"%str(translation))
                 (M.transform.rotation.x,
                  M.transform.rotation.y,
                  M.transform.rotation.z,
                  M.transform.rotation.w) = quaternion
-                pub_camera.publish (M)
+                pub_world.publish (M)
+                rate.sleep ()
             except:
-                pass
+                print ("No common time found.")
+        else:
+            if not listener.frameExists("roseball"):
+                print ("roseball frame does not seem to exist.")
+            if not listener.frameExists("world"):
+                print ("world frame does not seem to exist.")
 
 if __name__ == '__main__':
     try:
